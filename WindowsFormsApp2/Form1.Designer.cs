8d21000d9b6667c3191ecbd470243712a0905f7b
vano <sinkovvv1@gmail.com>
2023-03-27 22:20:51 +0300

    111

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Зберегти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(60, 169);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(510, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Tag = "ak";
+            this.checkBox1.Text = "ІНСТИТУТ АВТОМАТИКИ, КІБЕРНЕТИКИ ТА ОБЧИСЛЮВАЛЬНОЇ ТЕХНІКИ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(265, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(472, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(265, 120);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(164, 22);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(60, 195);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(328, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Tag = "az";
+            this.checkBox2.Text = "ІНСТИТУТ АГРОЕКОЛОГІЇ ТА ЗЕМЛЕУСТРОЮ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(60, 221);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(313, 20);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Tag = "ba";
+            this.checkBox3.Text = "ІНСТИТУТ БУДІВНИЦТВА ТА АРХІТЕКТУРИ";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(60, 247);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(482, 20);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Tag = "vgp";
+            this.checkBox4.Text = "ІНСТИТУТ ВОДНОГО ГОСПОДАРСТВА ТА ПРИРОДООБЛАШТУВАННЯ";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(60, 273);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(320, 20);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Tag = "em";
+            this.checkBox5.Text = "ІНСТИТУТ ЕКОНОМІКИ ТА МЕНЕДЖМЕНТУ";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(60, 299);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(191, 20);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Tag = "mi";
+            this.checkBox6.Text = "МЕХАНІЧНИЙ ІНСТИТУТ";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(60, 325);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(146, 20);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Tag = "prava";
+            this.checkBox7.Text = "ІНСТИТУТ ПРАВА";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(60, 351);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(239, 20);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Tag = "oz";
+            this.checkBox8.Text = "ІНСТИТУТ ОХОРОНИ ЗДОРОВ\'Я";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Прізвище";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ім\'я";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Побатькові";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Рік вступу";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 481);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Create email for stydent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static async Task<string> TranslateText(string text, string targetLanguage)
+        {
+            string endpoint = $"https://api.mymemory.translated.net/get?q={text}&langpair=uk|{targetLanguage}";
+            var client = new HttpClient();
+            var response = await client.GetAsync(endpoint);
+            var json = await response.Content.ReadAsStringAsync();
+            var translation = Newtonsoft.Json.JsonConvert.DeserializeObject<Translation>(json);
+            return translation.responseData.translatedText;
+        }
+        private  void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "MyTest1.txt");
+
+            string targetLanguage = "en";
+            string v = await TranslateText(textBox1.Text, targetLanguage);
+            string name = await TranslateText(textBox2.Text, targetLanguage);
+            string nameD = await TranslateText(textBox3.Text, targetLanguage);
+            string year = await TranslateText(textBox4.Text, targetLanguage);
+            string year1 = year.Substring(year.Length - 2);
+            string email = "";
+
+            // Check if the file already exists and create it if it doesn't
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+
+                    AddText(fs, "Дані студента: " + textBox1.Text.Substring(0, 1).ToUpper() + textBox1.Text.Substring(1) + " " + textBox2.Text.Substring(0, 1).ToUpper() + textBox2.Text.Substring(1) + " " + textBox3.Text.Substring(0, 1).ToUpper() + textBox3.Text.Substring(1) + "\n");
+                    AddText(fs, "Рік вступу: " + textBox4.Text + "\n");
+
+                    if (checkBox1.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox1.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox1.Text + "\n");
+                    }
+                    else if (checkBox2.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox2.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox2.Text + "\n");
+                    }
+                    else if (checkBox3.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox3.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox3.Text + "\n");
+                    }
+                    else if (checkBox4.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox4.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox4.Text + "\n");
+                    }
+                    else if (checkBox5.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox5.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox5.Text + "\n");
+                    }
+                    else if (checkBox6.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox6.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox6.Text + "\n");
+                    }
+                    else if (checkBox7.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox7.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox7.Text + "\n");
+                    }
+                    else if (checkBox8.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox8.Tag + year1 + "@nuwm.edu.ua";
+                        AddText(fs, "В якому інституті навчається: " + checkBox8.Text + "\n");
+
+                    }
+                    AddText(fs, "Електрона почта вступника: " + email + "\n");
+                    AddText(fs, "Пароль до почти вступника: " + name + "_" + year + "\n");
+                }
+            }
+            else // If the file already exists, append the new student's data to it
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine();
+                    sw.WriteLine("Дані студента: " + textBox1.Text.Substring(0, 1).ToUpper() + textBox1.Text.Substring(1) + " " + textBox2.Text.Substring(0, 1).ToUpper() + textBox2.Text.Substring(1) + " " + textBox3.Text.Substring(0, 1).ToUpper() + textBox3.Text.Substring(1));
+                    sw.WriteLine("Рік вступу: " + textBox4.Text);
+
+                    if (checkBox1.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox1.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox1.Text);
+                    }
+                    else if (checkBox2.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox2.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox2.Text);
+                    }
+                    else if (checkBox3.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox3.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox3.Text);
+                    }
+                    else if (checkBox4.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox4.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox4.Text);
+                    }
+                    else if (checkBox5.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox5.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox5.Text);
+                    }
+                    else if (checkBox6.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox6.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox6.Text);
+                    }
+                    else if (checkBox7.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox7.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox7.Text);
+                    }
+                    else if (checkBox8.Checked == true)
+                    {
+                        email = v.ToLower() + "." + name.Substring(0, 1).ToLower() + "_" + checkBox8.Tag + year1 + "@nuwm.edu.ua";
+                        sw.WriteLine("В якому інституті навчається: " + checkBox8.Text);
+                       
+                    }
+                    sw.WriteLine("Електрона почта вступника: " + email);
+                    sw.WriteLine("Пароль до почти вступника: " + name + "_" + year);
+                }
+            }
+             MessageBox.Show("Дані успішно записані в файл!");
+            // Open the file in a text editor
+            foreach (var process in Process.GetProcessesByName("notepad"))
+            {
+                process.Kill();
+            }
+            Process notepadProcess = Process.Start("notepad.exe", path);
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+      
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        class Translation
+        {
+            public ResponseData responseData { get; set; }
+        }
+
+        class ResponseData
+        {
+            public string translatedText { get; set; }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
